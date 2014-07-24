2d53fecde03ba0e282c5a970d898205286b40dca
Sohil Nandu <sohilnandu@gmail.com>
2014-07-24 16:50:17 -0700

    adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+json = ActiveSupport::JSON.decode(File.read('db/seeds/registrants.json'))
+
+json.each do |a|
+  EventRegistrant.create!({
+      :ImportID => a['ImportID'],
+      :FirstName => a['FirstName'],
+      :LastName => a['LastName'],
+      :Addr1 => a['Addr1'],
+      :Addr2 => a['Addr2'],
+      :City => a['City'],
+      :State => a['State'],
+      :DonorCategory => a['DonorCategory'],
+      :NextAskAmount => a['NextAskAmount'],
+      :TotalIdentifiedAssets => a['TotalIdentifiedAssets'],
+      :CurrentMajor1kDonor => a['CurrentMajor1kDonor'],
+      :MajorDonorWith1MMinAssets => a['MajorDonorWith1MMinAssets'],
+      :MajorUnderPerformer => a['MajorUnderPerformer'],
+      :MajorUnderPerformingByAmount => a['MajorUnderPerformingByAmount'],
+      :AnnualDonorWith1MMinAssets => a['AnnualDonorWith1MMinAssets'],
+      :AnnualUnderPerformer => a['AnnualUnderPerformer'],
+      :AnnualUnderPerformingByAmount => a['AnnualUnderPerformingByAmount']
+  })
+end
